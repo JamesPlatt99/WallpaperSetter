58ced97531447fd5cec906fc5ad0d72383c9184f
James Platt <James.Platt@neptune.com>
2018-05-06 22:33:57 +0100

    Initial commit

---
+++ Wallpaper Picker/ImageRetriever.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wallpaper_Picker
+{
+    class ImageRetriever
+    {
+        #region enumeration
+        public enum Source
+        {
+            earthPorn = 1,
+            spacePorn = 2,
+            wallpapers = 3
+        }
+
+        #endregion
+
+        #region properties
+        private int _selectedSource;
+        public string _selectedSourceURL {
+            get
+            {
+                switch (_selectedSource) {
+                    case 1:
+                        return "https://www.reddit.com/r/EarthPorn/";
+                    case 2:
+                        return "https://www.reddit.com/r/spaceporn/";
+                    case 3:
+                        return "https://www.reddit.com/r/wallpapers/";
+                    default:
+                        Random rnd = new Random();
+                        _selectedSource = rnd.Next(1, 3);
+                        return _selectedSourceURL;
+                }
+            }
+        }
+        #endregion
+
+        #region constructor
+
+        public ImageRetriever(Source selectedSource)
+        {
+            _selectedSource = (int) selectedSource;
+        }
+
+        #endregion
+
+        #region public methods
+
+        #endregion
+
+        #region helper methods
+
+        #endregion
+
+    }
+}
---
+++ Wallpaper Picker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wallpaper_Picker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageRetriever = new ImageRetriever(SelectRandomSource());
+        }
+
+        static ImageRetriever.Source SelectRandomSource()
+        {
+            var random = new Random();
+            int selectedSource = random.Next(1, Enum.GetNames(typeof(ImageRetriever.Source)).Length);
+            return (ImageRetriever.Source)selectedSource;
+        }
+    }
+}
